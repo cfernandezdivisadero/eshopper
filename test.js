54a0575ba941563d2505f57fc6a70d3f3acb82ff
cfernandezdivisadero <cfernandez@divisadero.es>
2020-03-03 18:09:20 +0100

    Added Tealium code

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 console.log('Creado desde GIT from Develop');
+
+jQuery('.add-to-cart').on('click', function() {
+    var name = jQuery(this).parent().find('p').text();
+    var id = Math.floor(Math.random()*1000);
+     utag.link({
+        product_ids: [id],
+        product_names: [name],
+        accionEcommerce: 'add'
+    }, function(){}, [5]);
+});
